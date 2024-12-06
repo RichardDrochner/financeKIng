ebf9b08707675acc8ef1fdf00f865e565fdca36f
RichardDrochner <len_drochner@web.de>
2024-12-06 15:29:54 +0100

    Token in ENV_Variable gespeichert

--- Llama.py
+++ Llama.py
@@ -1,6 +1,8 @@
 from huggingface_hub import snapshot_download
-
+import os
 # Lade das Modell herunter
+
+hf_token = os.getenv('HF_TOKEN')
 snapshot_download(
     repo_id="meta-llama/Llama-3.2-1B",
     cache_dir="./models",
